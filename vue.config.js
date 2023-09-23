dd6533e1664f6c3063952653e492493d59683ee3
gonzapf27 <gonzalopg_mieres@hotmail.com>
2023-09-23 21:42:49 +0200

    Primer commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/gonzapf27.github.io/' : '/',
   transpileDependencies: true
 })
